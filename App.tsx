5e087bc3f1fa8aaf66a97e663801cb39ddc126f4
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2024-08-11 04:10:15 +0530

    set up done

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
